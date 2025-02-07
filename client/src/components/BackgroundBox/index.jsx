b16729d1b7cc49d0b06b1658bbeef2210f55588b
fdanctl <daniel.a.d.freire.00@gmail.com>
2025-02-07 11:14:02 +0000

    new design

--- client/src/components/BackgroundBox/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import styles from "./styles.module.css";
-
-const BackgroundBox = ({ children }) => {
-  return <div className={styles.backgroundBox}>{children}</div>;
-};
-
-export default BackgroundBox;
