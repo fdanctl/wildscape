85eb3987d93160818da1d490ec683aa9107338d6
fdanctl <daniel.a.d.freire.00@gmail.com>
2025-02-07 15:02:45 +0000

    set fonts and colors

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,11 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primaryGreen: "#344E41",
+        secundaryGreen: "#A3B18A",
+        grayish: "#DAD7CD",
+        redish: "#782929",
+      },
+      fontFamily: {
+        heading: ["Zilla Slab", "serif"],
+        body: ["Lato", "Arial", "sans-serif"],
+      },
+    },
   },
   plugins: [],
-}
-
+};
